5e1eff62547e3a5b770892b96673392d8a83ca36
Satoko Shimizu <satokoshimizu.19@gmail.com>
2018-09-23 09:18:11 -0700

    making calendar to generate dates

--- assets/calendar/calendar.js
+++ assets/calendar/calendar.js
@@ -13,7 +13,38 @@ let calendar = () => {
   //Use const rather than let or var, we use const when a variable doesn't get reassigned or updated overtime.
   //Since these are functions, we don't expect the functions to change so we can make them constants.
 
+const currentTime = new Date()
 
+// returns the month (from 0 to 11)
+const currentMonth = currentTime.getMonth() + 1
+
+// returns the day of the month (from 1 to 31)
+const currentDay = currentTime.getDate()
+
+// returns the year (four digits)
+const currentYear = currentTime.getFullYear()
+
+// write output MM/dd/yyyy
+document.write(month + "/" + day + "/" + year)
+
+var lastMonth = function(year, month, dayOfWeek) {
+  let previousMonth = month-1;
+  let daysInPreviousMonth = function(y, p){
+    let previousMonthArr = [];
+    let ld = lastDay(y, p);
+    let i = 0;
+    while(i < daysOfTheWeek) {
+      previousMonthArr.push(ld);
+      ld--;
+      i++;
+    }
+    return previousMonthArr;
+  }
+  if(month < 1){
+    previousMonth = 12;
+  }
+  return daysInPreviousMonth(year-1, previousMonth;
+};
   //TODO: Create the lastMonth function which takes the arguments, "year", "month", "dayOfWeek"
   //Create the previousMonth variable which is equal to month-1
   //Create the daysInPreviousMonth function which takes two arguments, "y", "p".
@@ -37,10 +68,6 @@ let calendar = () => {
     //return the daysInPreviousMonth function which takes the arguments year and previousMonth
 //end lastMonth function
 
-
-
-
-
   const daysOfTheWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
 
   const calendarHead = document.getElementById('calendar-head');
@@ -57,6 +84,7 @@ let calendar = () => {
   const populateCalendarBody = () => {
     const currentDaysInMonth = lastDay(2018, 9);
     const firstDayOfMonth = firstDay(2018, 9);
+    const lastMonthArr = lastMonth(currentYear, currentMonth, firstDayOfMonth);
     //TODO: Create lastMonthArr const which is equal to the lastMonth function which
     // takes the arguments currentYear, currentMonth, and firstDayOfMonth
     const daysInRow = 7;
@@ -65,11 +93,18 @@ let calendar = () => {
     let currentDayOfMonthIndex = 1; //current day of current month (e.g. september)
     let currentCalendarDayIndex = 0; //current index of days on calendar (i.e. daysInRow * totalRows)
     //TODO: Create lmaIndex variable which is equal to the firstDayOfMonth-1
+    var lmaIndex = firstDayOfMonth-1;
 
     while(currentRows < totalRows){
       const calendarRow = document.createElement('div');
       for(let i = 0; i<daysInRow; i++){
         const day = document.createElement('p');
+        if (currentCalendarDayIndex < firstDayOfMonth){
+          lmaIndex.innerhtml = lastMonthArr;
+          day.classList.add('grayedOut');
+          lmaIndex--;
+        }
+      }
         //TODO: Create a new if condition which says
         //"If the currentCalendarDayIndex is less than the firstDayOfMonth"
           //Within the if condition
